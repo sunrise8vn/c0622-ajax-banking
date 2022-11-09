da9ab597a8ea088ada6f31834388688c2980b786
sunrise8vn <sunrise8vn@gmail.com>
2022-11-09 15:23:05 +0700

    refactor code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
+class App {
+    static DOAMIN_SERVER = "http://localhost:3003";
+    static CUSTOMER_API = this.DOAMIN_SERVER + "/customers";
+    static DEPOSIT_API = this.DOAMIN_SERVER + "/deposits";
+}
+
 class Customer {
     constructor(id, fullName, email, phone, address, balance, deleted) {
         this.id = id;
